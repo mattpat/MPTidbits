83580f7fcb6c62aafcfd305506d0c4da5022e389
Matt Patenaude <MattPat@mattpat.net>
2009-11-06 15:34:43 -0500

    fixed a potential buffer overrun on the hash functions

--- NSString+MPTidbits.m
+++ NSString+MPTidbits.m
@@ -47,14 +47,14 @@
 - (NSString *)MD5Hash
 {
 	const char *input = [self UTF8String];
-	unsigned char result[16];
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
 	CC_MD5(input, strlen(input), result);
 	return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]];
 }
 - (NSString *)SHA1Hash
 {
 	const char *input = [self UTF8String];
-	unsigned char result[16];
+	unsigned char result[CC_SHA1_DIGEST_LENGTH];
 	CC_SHA1(input, strlen(input), result);
 	return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17], result[18], result[19]];
 }

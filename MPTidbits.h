80fda2c643fbab85e0f79da8869c31f101eaa9d8
Matt Patenaude <MattPat@mattpat.net>
2009-11-06 18:43:05 -0500

    merged hex string code into its own function, better reusability

--- MPTidbits.h
+++ MPTidbits.h
@@ -25,5 +25,6 @@
  *
  */
 
+#import "MPFunctions.h"
 #import "NSString+MPTidbits.h"
 #import "NSCollections+MPTidbits.h"
--- NSString+MPTidbits.m
+++ NSString+MPTidbits.m
@@ -25,6 +25,7 @@
 //
 
 #import "NSString+MPTidbits.h"
+#import "MPFunctions.h"
 #import <CommonCrypto/CommonDigest.h>
 
 
@@ -49,14 +50,14 @@
 	const char *input = [self UTF8String];
 	unsigned char result[CC_MD5_DIGEST_LENGTH];
 	CC_MD5(input, strlen(input), result);
-	return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]];
+	return MPHexStringFromBytes(result, CC_MD5_DIGEST_LENGTH);
 }
 - (NSString *)SHA1Hash
 {
 	const char *input = [self UTF8String];
 	unsigned char result[CC_SHA1_DIGEST_LENGTH];
 	CC_SHA1(input, strlen(input), result);
-	return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17], result[18], result[19]];
+	return MPHexStringFromBytes(result, CC_SHA1_DIGEST_LENGTH);
 }
 
 @end
